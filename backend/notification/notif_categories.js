b6678b8d9e436841153bb8633d4d0623210e655c
Izuchukwu Mba <izuchukwu@meta.com>
2024-07-17 15:22:40 -0700

    Duplicate file fix

--- backend/notification/notif_categories.js
+++
@@ -1,6 +0,0 @@
-export const notif_categories = {
-  added_to_group: "added_to_group",
-  file_created: "file_created",
-  file_updated: "file_updated",
-  file_deleted: "file_deleted",
-};
---
+++ backend/notification/notif_categories_backend.js
@@ -0,0 +1,16 @@
+const notif_categories = {
+  added_to_group: "added_to_group",
+  file_created: "file_created",
+  file_updated: "file_updated",
+  file_deleted: "file_deleted",
+};
+
+const socket_names = {
+  connection: "connection",
+  register: "register",
+  rate_limit: "rate-limit",
+  group_notifications: "notify_group",
+  added_user_to_group: "added_user_to_group",
+};
+
+module.exports = { notif_categories, socket_names };
